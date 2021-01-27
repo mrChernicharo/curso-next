e1dc709841694bdde9e68459cd05b2ae68724e01
Felipe Chernicharo <string7dev@gmail.com>
2021-01-26 22:13:37 -0300

    roteamento com Next pt 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import Test from "./test";
 export default function Home() {
   return (
     <div>
       <h1>Hello world</h1>
+
+      <br />
+
+      <Test number="4" message="testando!" />
     </div>
   );
 }
---
+++ pages/test.tsx
@@ -0,0 +1,9 @@
+export default function Test({ number, message }) {
+  return (
+    <div>
+      <p>Test</p>
+      <p>{number}</p>
+      <p>{message}</p>
+    </div>
+  );
+}
